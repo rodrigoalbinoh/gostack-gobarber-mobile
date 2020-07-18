28f8ecbb5b76212ac6fa6cfa36b979dd4dae99e0
Rodrigo <rodrigo.hammes7@gmail.com>
2020-07-18 16:47:48 -0300

    Dashboard: implementado Header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,15 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+  email: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -19,7 +25,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
   loading: boolean;
